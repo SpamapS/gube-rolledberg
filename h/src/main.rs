fa0592edddbf1329d14a05d0623177be9496f7d6
Clint Byrum <clint@fewbar.com>
2021-06-19 19:35:31 -0700

    h complete

--- h/src/main.rs
+++ h/src/main.rs
@@ -1,19 +1,33 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
 
 fn get_h() -> std::io::Result<char> {
     // What ratio of words start with h?
     let dict: File = File::open("/etc/dictionaries-common/words")?;
     let reader = BufReader::new(dict);
     let mut n_lines = 0;
+    let mut n_h_lines = 0;
+    let mut letter_counts: HashMap<char, usize> = HashMap::new();
     for line in reader.lines() {
-        n_lines += 1;     
-        if n_lines == 69 {
-            println!("nice: {}", line.unwrap());
+        match line {
+            Ok(line) => {
+                if let Some(c) = line.chars().nth(0) {
+                    n_lines += 1;     
+                    let l_c = c.to_lowercase().next().unwrap();
+                    if l_c == 'h' {
+                        n_h_lines += 1;
+                    }
+                    letter_counts.entry(l_c).and_modify(|e| { *e += 1}).or_insert(1);
+                }
+            },
+            Err(e) => return Err(e)
         }
     };
-    println!("{}", n_lines);
-    return Ok('h');
+    let h_ratio  = n_h_lines as f32 / n_lines as f32;
+    let entries: Vec<(char, usize)> = letter_counts.drain().filter(|e| e.1 as f32 / n_lines as f32 == h_ratio).collect::<Vec<(char, usize)>>();
+    return Ok(entries[0].0);
 }
 
 fn main() -> std::io::Result<()> {
